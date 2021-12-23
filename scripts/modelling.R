14eef3ee1289036005709bab19c562cb4094c11e
saulosgil <saulosgil@hotmail.com>
2021-12-23 09:44:31 -0300

    - novas analises

--- scripts/modelling.R
+++ scripts/modelling.R
@@ -1,18 +1,16 @@
 # Pacotes ------------------------------------------------------------------
-
 library(tidymodels)
 library(tidyverse)
 library(pROC)
 library(vip)
 library(randomForest)
 
-
 # Bases de dados ----------------------------------------------------------
-base <- readr::read_rds("base.rds")
+base <- readr::read_rds("data/base.rds")
 
 glimpse(base)
 
-base |> count(heart_disease)
+base |> count(heart_disease) # desfecho (parece balanceado!)
 
 # Base de treino e teste --------------------------------------------------
 
@@ -27,7 +25,6 @@ base_test  <- testing(base_initial_split)
 base_resamples <- vfold_cv(base_train, v = 5, strata = "heart_disease")
 
 # Exploratória ------------------------------------------------------------
-
 skimr::skim(base_train)
 
 visdat::vis_miss(base_train)
@@ -43,13 +40,13 @@ base_train |>
 ## Data prep
 base_lr_recipe <- recipe(heart_disease ~ ., data = base_train) |>
   step_normalize(all_numeric_predictors()) |>
-  # step_novel(all_nominal_predictors()) |>
+  step_novel(all_nominal_predictors()) |>
   # step_impute_linear(Income, Assets, Debt, impute_with = imp_vars(Expenses)) %>%
   # step_impute_mode(all_nominal_predictors()) |>
   step_zv(all_predictors()) |>
-  step_poly(all_numeric_predictors(), degree = 1) |>
-  step_corr(all_numeric_predictors())
-  # step_dummy(all_nominal_predictors())
+  step_poly(all_numeric_predictors(), degree = 9) |>
+  step_corr(all_numeric_predictors()) |>
+  step_dummy(all_nominal_predictors())
 
 bake(prep(base_lr_recipe), new_data = NULL)
 
@@ -93,7 +90,8 @@ collect_metrics(base_lr_tune_grid)
 ## Data prep
 
 base_dt_recipe <- recipe(heart_disease ~ ., data = base) |>
- # step_novel(all_nominal_predictors()) %>%
+  step_normalize(all_numeric_predictors()) |>
+ step_novel(all_nominal_predictors()) |>
   step_zv(all_predictors()) |>
   step_corr(all_numeric_predictors())
 
@@ -141,6 +139,7 @@ collect_metrics(base_dt_tune_grid)
 ## Data prep
 
 base_rf_recipe <- recipe(heart_disease ~ ., data = base) |>
+  step_normalize(all_numeric_predictors()) |>
   step_zv(all_predictors()) |>
   step_corr(all_numeric_predictors())
 
@@ -162,9 +161,9 @@ base_rf_wf <- workflow() |>
 ## Tune
 
 grid_rf <- grid_random(
-  min_n(range = c(5, 25)),
-  mtry(range = c(10, 18)),
-  trees(range = c(20, 400))
+  min_n(range = c(20, 80)),
+  mtry(range = c(4, 18)),
+  trees(range = c(200, 400))
 )
 
 # doParallel::registerDoParallel(4)
@@ -181,6 +180,381 @@ base_rf_tune_grid <- tune_grid(
 autoplot(base_rf_tune_grid)
 collect_metrics(base_rf_tune_grid)
 
+# XGBoost -----------------------------------------------------------------
+
+## Data prep
+
+base_xb_recipe <- recipe(heart_disease ~ ., data = base)|>
+  step_normalize(all_numeric_predictors()) |>
+  step_novel(all_nominal_predictors()) |>
+  # step_impute_linear(Income, Assets, Debt, impute_with = imp_vars(Expenses)) %>%
+  # step_impute_mode(all_nominal_predictors()) |>
+  step_zv(all_predictors()) |>
+  step_poly(all_numeric_predictors(), degree = 9) |>
+  step_corr(all_numeric_predictors()) |>
+  step_dummy(all_nominal_predictors())
+
+## Estratégia de Tunagem de Hiperparâmetros
+
+# Achar uma combinação `learning_rate` e `trees` que funciona relativamente bem. Usando uma learning_rate alta. Vamos fixar os valores dos outros parâmetros.
+#
+# -   `min_n`: usar um valor entre 1 e 30 é razoável no começo.
+# -   `max_depth`: geralmente começamos com algo entre 4 e 6.
+# -   `loss_reduction`: vamos começar com 0, geralmente começamos com valores baixos.
+# -   `mtry`: começamos com +- 80% do número de colunas na base.
+# -   `sample_size`: também fazemos approx 80% do número de linhas.
+#
+# Em seguida vamos tunar o `learn_rate` e `trees` em um grid assim:
+#
+# -   `learn_rate` - 0.05, 0.1, 0.3
+
+# -   `trees` - 100, 500, 1000, 1500
+
+# ### Passo 1:
+
+# Achar uma combinação `learning_rate` e `trees` que funciona relativamente bem. Usando uma learning_rate alta.
+# Vamos fixar os valores dos outros parâmetros.
+
+## Modelo
+
+base_xb_model <- boost_tree(
+  min_n = 15,
+  mtry = 0.8,
+  trees = tune(),
+  tree_depth = 4,
+  learn_rate = tune(),
+  loss_reduction = 0,
+  sample_size = 0.8
+) |>
+  set_mode("classification") |>
+  set_engine("xgboost",count = FALSE)
+
+## Workflow
+
+base_xb_wf <- workflow() |>
+  add_model(base_xb_model) |>
+  add_recipe(base_xb_recipe)
+
+## Tune grid para learn rate e trees
+
+grid_xb <- expand.grid(
+  trees = c(250, 500, 1000),
+  learn_rate = c(0.05, 0.3)
+)
+
+grid_xb
+
+# doParallel::registerDoParallel(4)
+
+base_xb_tune_grid <- tune_grid(
+  base_xb_wf,
+  resamples = base_resamples,
+  grid = grid_xb,
+  metrics = metric_set(roc_auc)
+)
+
+#### Melhores hiperparâmetros
+
+autoplot(base_xb_tune_grid)
+show_best(base_xb_tune_grid, n = 6)
+
+base_select_xgb_best_passo1 <- base_xb_tune_grid |>
+  select_best(metric = "roc_auc")
+
+base_select_xgb_best_passo1
+
+## Passo 2
+
+base_select_xgb_best_passo1$trees
+base_select_xgb_best_passo1$learn_rate
+
+# Vimos que com os parâmetros da árvore fixos:
+#
+# -   `trees` = `r telco_select_best_passo1$trees`
+# -   `learn_rate` = `r telco_select_best_passo1$learn_rate`
+#
+# São bons valores inciais. Agora, podemos tunar os parâmetros relacionados à árvore.
+#
+# -   `tree_depth`: vamos deixar ele variar entre 3 e 10.
+# -   `min_n`: vamos deixar variar entre 5 e 90.
+#
+# Os demais deixamos fixos como anteriormente.
+
+## Modelo
+
+base_xb_model <- boost_tree(
+  min_n = tune(),
+  mtry = 0.8,
+  trees = base_select_xgb_best_passo1$trees,
+  tree_depth = tune(),
+  learn_rate = base_select_xgb_best_passo1$learn_rate,
+  loss_reduction = 0,
+  sample_size = 0.8
+) |>
+  set_mode("classification") |>
+  set_engine("xgboost", count = FALSE)
+
+## Workflow
+
+base_xb_wf <- workflow() |>
+  add_model(base_xb_model) |>
+  add_recipe(base_xb_recipe)
+
+## Tune grid para learn rate e trees
+
+grid_xb <- expand.grid(
+  tree_depth = c(3, 4, 6),
+  min_n = c(30, 60, 90)
+)
+
+grid_xb
+
+# doParallel::registerDoParallel(4)
+
+base_xb_tune_grid <- tune_grid(
+  base_xb_wf,
+  resamples = base_resamples,
+  grid = grid_xb,
+  metrics = metric_set(roc_auc)
+)
+
+#### Melhores hiperparâmetros
+
+autoplot(base_xb_tune_grid)
+show_best(base_xb_tune_grid, n = 6)
+
+base_select_xgb_best_passo2 <- base_xb_tune_grid |>
+  select_best(metric = "roc_auc")
+
+base_select_xgb_best_passo2
+
+### Passo 3:
+
+# Agora também temos definidos:
+
+base_select_xgb_best_passo2$tree_depth
+base_select_xgb_best_passo2$min_n
+
+# -   `trees` = `r telco_select_best_passo1$trees`
+# -   `learn_rate` = `r telco_select_best_passo1$learn_rate`
+# -   `min_n` = `r telco_select_best_passo2$min_n`
+# -   `tree_depth` = `r telco_select_best_passo2$tree_depth`
+#
+# Vamos então tunar o `loss_reduction`:
+#
+#   `loss_reduction`: vamos deixar ele variar entre 0 e 2
+
+## Modelo
+
+base_xb_model <- boost_tree(
+  min_n = base_select_xgb_best_passo2$min_n,
+  mtry = 0.8,
+  trees = base_select_xgb_best_passo1$trees,
+  tree_depth = base_select_xgb_best_passo2$tree_depth,
+  learn_rate = base_select_xgb_best_passo1$learn_rate,
+  loss_reduction = tune(),
+  sample_size = 0.8
+) |>
+  set_mode("classification") |>
+  set_engine("xgboost", count = FALSE)
+
+## Workflow
+
+base_xb_wf <- workflow() |>
+  add_model(base_xb_model) |>
+  add_recipe(base_xb_recipe)
+
+## Tune grid para learn rate e trees
+
+grid_xb <- expand.grid(
+  loss_reduction = c(0, 0.05, 1, 1.5, 2)
+)
+
+grid_xb
+
+# doParallel::registerDoParallel(4)
+
+base_xb_tune_grid <- tune_grid(
+  base_xb_wf,
+  resamples = base_resamples,
+  grid = grid_xb,
+  metrics = metric_set(roc_auc)
+)
+
+#### Melhores hiperparâmetros
+
+autoplot(base_xb_tune_grid)
+show_best(base_xb_tune_grid, n = 6)
+
+base_select_xgb_best_passo3 <- base_xb_tune_grid |>
+  select_best(metric = "roc_auc")
+
+base_select_xgb_best_passo3
+
+### Passo 4:
+
+# Não parece que o `lossreduction` teve tanto efeito, mas, vamos usar "base_select_xgb_best_passo3$loss_reduction" que deu o melhor resultado. Até agora temos definido:
+
+base_select_xgb_best_passo3$loss_reduction
+
+# -   `trees` = `r telco_select_best_passo1$trees`
+# -   `learn_rate` = `r telco_select_best_passo1$learn_rate`
+# -   `min_n` = `r telco_select_best_passo2$min_n`
+# -   `tree_depth` = `r telco_select_best_passo2$tree_depth`
+# -   `lossreduction` = `r telco_select_best_passo3$loss_reduction`
+#
+# Vamos então tunar o `mtry` e o `sample_size`:
+#
+#   -   `mtry`: de 10% a 100%
+#   -   `sample_size`: de 50% a 100%
+
+## Modelo
+
+base_xb_model <- boost_tree(
+  min_n = base_select_xgb_best_passo2$min_n,
+  mtry = tune(),
+  trees = base_select_xgb_best_passo1$trees,
+  tree_depth = base_select_xgb_best_passo2$tree_depth,
+  learn_rate = base_select_xgb_best_passo1$learn_rate,
+  loss_reduction = base_select_xgb_best_passo3$loss_reduction,
+  sample_size = tune()
+) |>
+  set_mode("classification") |>
+  set_engine("xgboost", count = FALSE)
+
+## Workflow
+
+base_xb_wf <- workflow() |>
+  add_model(base_xb_model) |>
+  add_recipe(base_xb_recipe)
+
+## Tune grid para learn rate e trees
+
+grid_xb <- expand.grid(
+  sample_size = seq(0.5, 1.0, length.out = 2),
+  mtry = seq(0.1, 1.0, length.out = 2)
+)
+
+grid_xb
+
+# doParallel::registerDoParallel(4)
+
+base_xb_tune_grid <- tune_grid(
+  base_xb_wf,
+  resamples = base_resamples,
+  grid = grid_xb,
+  metrics = metric_set(roc_auc)
+)
+
+#### Melhores hiperparâmetros
+
+autoplot(base_xb_tune_grid)
+show_best(base_xb_tune_grid, n = 6)
+
+base_select_xgb_best_passo4 <- base_xb_tune_grid |>
+  select_best(metric = "roc_auc")
+
+base_select_xgb_best_passo4
+
+
+### Passo 5:
+
+base_select_xgb_best_passo4$mtry
+
+base_select_xgb_best_passo4$sample_size
+
+# Vimos que a melhor combinação foi
+#
+# -   `mtry` = `r telco_select_best_passo4$mtry`
+# -   `sample_size` = `r telco_select_best_passo4$sample_size`
+#
+# Agora vamos tunar o `learn_rate` e o `trees` de novo, mas deixando o `learn_rate` assumir valores menores.
+
+## Modelo
+
+base_xb_model <- boost_tree(
+  min_n = base_select_xgb_best_passo2$min_n,
+  mtry = base_select_xgb_best_passo4$mtry,
+  trees = tune(),
+  tree_depth = base_select_xgb_best_passo2$tree_depth,
+  learn_rate = tune(),
+  loss_reduction = base_select_xgb_best_passo3$loss_reduction,
+  sample_size = base_select_xgb_best_passo4$sample_size
+) |>
+  set_mode("classification") |>
+  set_engine("xgboost", count = FALSE)
+
+## Workflow
+
+base_xb_wf <- workflow() |>
+  add_model(base_xb_model) |>
+  add_recipe(base_xb_recipe)
+
+## Tune grid para learn rate e trees
+
+grid_xb <- expand.grid(
+  learn_rate = c(0.05),
+  trees = c(100, 600, 750)
+)
+
+grid_xb
+
+# doParallel::registerDoParallel(4)
+
+base_xb_tune_grid <- tune_grid(
+  base_xb_wf,
+  resamples = base_resamples,
+  grid = grid_xb,
+  metrics = metric_set(roc_auc)
+)
+
+#### Melhores hiperparâmetros
+
+autoplot(base_xb_tune_grid)
+show_best(base_xb_tune_grid, n = 6)
+
+base_select_xgb_best_passo5 <- base_xb_tune_grid |>
+  select_best(metric = "roc_auc")
+
+base_select_xgb_best_passo5
+
+## Desempenho do Modelo Final
+
+base_xb_model <- boost_tree(
+  min_n = base_select_xgb_best_passo2$min_n,
+  mtry = base_select_xgb_best_passo4$mtry,
+  trees = base_select_xgb_best_passo5$trees,
+  tree_depth = base_select_xgb_best_passo2$tree_depth,
+  learn_rate = base_select_xgb_best_passo5$learn_rate,
+  loss_reduction = base_select_xgb_best_passo3$loss_reduction,
+  sample_size = base_select_xgb_best_passo4$sample_size
+) |>
+  set_mode("classification") |>
+  set_engine("xgboost", count = FALSE)
+
+## Workflow
+
+base_xb_wf <- workflow() |>
+  add_model(base_xb_model) |>
+  add_recipe(base_xb_recipe)
+
+base_last_fit <- base_xb_wf |>
+  last_fit(
+    split = base_initial_split,
+    control = control_grid(save_pred = TRUE, verbose = FALSE, allow_par = TRUE),
+    metrics = metric_set(roc_auc, f_meas, accuracy, precision, recall)
+  )
+
+#### Métricas
+collect_metrics(base_last_fit)
+
+#### Variáveis Importantes
+
+base_last_fit |>
+  pluck(".workflow", 1) |>
+  pull_workflow_fit() |>
+  vip::vip(num_features = 20)
+
 # Desempenho dos modelos finais ----------------------------------------------
 
 base_lr_best_params <- select_best(base_lr_tune_grid, "roc_auc")
@@ -195,10 +569,15 @@ base_rf_best_params <- select_best(base_rf_tune_grid, "roc_auc")
 base_rf_wf <- base_rf_wf |>  finalize_workflow(base_rf_best_params)
 base_rf_last_fit <- last_fit(base_rf_wf, base_initial_split)
 
+base_xb_best_params <- select_best(base_xb_tune_grid, "roc_auc")
+base_xb_wf <- base_xb_wf |>  finalize_workflow(base_xb_best_params)
+base_xb_last_fit <- last_fit(base_xb_wf, base_initial_split)
+
 base_test_preds <- bind_rows(
-  collect_predictions(base_lr_last_fit) |> mutate(modelo = "LinearRegression"),
+  collect_predictions(base_lr_last_fit) |> mutate(modelo = "LogisticRegression"),
   collect_predictions(base_dt_last_fit) |> mutate(modelo = "DecisionTree"),
-  collect_predictions(base_rf_last_fit) |> mutate(modelo = "RandomForest")
+  collect_predictions(base_rf_last_fit) |> mutate(modelo = "RandomForest"),
+  collect_predictions(base_xb_last_fit) |> mutate(modelo = "XGBoost")
 )
 
 ## roc
@@ -229,13 +608,13 @@ write_rds(base_lr_last_fit, "base_lr_last_fit.rds")
 write_rds(base_lr_model, "base_lr_model.rds")
 
 
-# Modelo final ------------------------------------------------------------
+# Testando o Modelo final -------------------------------------------------
 
-base_final_lr_model <- base_lr_wf |> fit(base)
+base_final_xb_model <- base_lr_wf |> fit(base)
 
 test1 <- base[4,]
 
-predict(base_final_lr_model, new_data = base)
+predict(base_final_lr_model, new_data = base)[4,]
 
 
 
--- scripts/pre_processing.R
+++ scripts/pre_processing.R
@@ -1,6 +1,8 @@
 # Pacotes -----------------------------------------------------------------
 library(readr)
 library(tidyverse)
+library(tidymodels)
+library(GGally)
 
 # Lendo a base ------------------------------------------------------------
 heart <- read_csv("data/heart.csv") |>
@@ -8,7 +10,7 @@ heart <- read_csv("data/heart.csv") |>
 
 # Variável resposta - Heart Disease ---------------------------------------
 heart |>
-  ggplot(mapping = aes(x = heart_disease, fill = heart$sex)) +
+  ggplot(mapping = aes(x = heart_disease, fill = sex)) +
   geom_bar()
 
 heart |> count(heart_disease)
@@ -19,8 +21,8 @@ glimpse(heart) # base bruta
 heart_adjust <- heart |>
   mutate(
     # ajustando o sexo
-    sex = case_when(sex == "M" ~ 0,
-                    sex == "F" ~ 1),
+    sex = case_when(sex == "M" ~ "0",
+                    sex == "F" ~ "1"),
     # criando 4 dummies para chest_pain_type que apresenta 4 categorias
     chest_pain_type_ATA = case_when(chest_pain_type == "ATA" ~ 1,
                                     chest_pain_type != "ATA" ~ 0),
@@ -69,7 +71,20 @@ heart_adjust <- heart |>
          st_slope_down,
          heart_disease
          ) |>
-  mutate(heart_disease = as.factor(heart_disease)
+  mutate(sex = as.character(sex),
+         fasting_bs = as.character(fasting_bs),
+         chest_pain_type_ATA = as.character(chest_pain_type_ATA),
+         chest_pain_type_NAP = as.character(chest_pain_type_NAP),
+         chest_pain_type_ASY = as.character(chest_pain_type_ASY),
+         chest_pain_type_TA = as.character(chest_pain_type_TA),
+         resting_ecg_normal = as.character(resting_ecg_normal),
+         resting_ecg_st = as.character(resting_ecg_st),
+         resting_ecg_lvh = as.character(resting_ecg_lvh),
+         exercise_angina = as.character(exercise_angina),
+         st_slope_up = as.character(st_slope_up),
+         st_slope_flat = as.character(st_slope_flat),
+         st_slope_down = as.character(st_slope_down),
+         heart_disease = as.character(heart_disease)
          )
 
 glimpse(heart_adjust)
@@ -79,10 +94,53 @@ skimr::skim(heart_adjust)
 
 visdat::vis_miss(heart_adjust) # missing data
 
-heart_adjust %>%
-  select(where(is.numeric)) %>%
-  cor(use = "pairwise.complete.obs") %>%
+# Correlações - heart disease vs numerics
+
+heart_adjust |>
+  select(age,
+         resting_bp,
+         cholesterol,
+         max_hr,
+         oldpeak) |>
+  cor(use = "pairwise.complete.obs") |>
   corrplot::corrplot()
 
+heart_adjust |>
+  select(c(where(is.numeric), heart_disease)) |>
+  ggpairs(aes(colour = heart_disease))
+
+# Descritivas - vars vs heart disease
+
+contagens_numer <- heart_adjust |>
+  select(c(where(is.numeric)),
+         heart_disease) |>
+  pivot_longer(-heart_disease, names_to = "variavel", values_to = "valor") %>%
+  count(heart_disease, variavel, valor)
+
+# Descritivas - heart_disease vs numerics
+
+contagens_numer |>
+  ggplot(aes(y = heart_disease, x = valor, fill = heart_disease)) +
+  geom_boxplot() +
+  facet_wrap(~variavel, scales = "free_x") +
+  # scale_x_log10() +
+  ggtitle("heart_disease vs. Variáveis Numéricas")
+
+# Descritivas - heart_disease vs categóricas
+
+contagens_categ <- heart_adjust |>
+  select(c(!where(is.numeric)),
+         heart_disease) |>
+  pivot_longer(-heart_disease, names_to = "variavel", values_to = "valor") |>
+  count(heart_disease, variavel, valor)
+
+contagens_categ |>
+  ggplot(aes(y = valor, x = n, fill = heart_disease)) +
+  geom_col(position = "fill") +
+  geom_label(aes(label = n), position = position_fill(vjust = 0.5)) +
+  facet_wrap(~variavel, scales = "free_y", ncol = 3) +
+  ggtitle("heart_disease vs. Variáveis Categóricas")
+
 # Nova base de dados ------------------------------------------------------
-saveRDS(object = heart_adjust,file = "base.rds")
+saveRDS(object = heart_adjust,file = "data/base.rds")
+
